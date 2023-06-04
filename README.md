ecb65ea0aa07d7af4ff46d2ac0c4f0b34e3935a3
secretariuss <secretarius@ukr.net>
2023-06-04 02:27:46 -0300

    remove html div added to js

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Work Day Scheduler Starter Code
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Description
+
+ Work Day Scheduler starts when the start button is pressed. It is necessary to give answers to the questions. If the answer is wrong then the time is subtracted from the timer. When all the questions are answered or the timer reaches 0 and the game is over. User can save their result and display the result on the screen.
+
+## Gitpage reference
+https://
+
+## Screen shots
+
+The following scrshots shows the web application's:
+
+![scrshot1](./assets/images/pg1.GIF)
+![scrshot2](./assets/images/pg2.PNG)
+
+## Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours of 9am to 5pm
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
--- script.js
+++ script.js
@@ -1,23 +1,295 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+var container = $('#container')
+var date = dayjs();
+var currentHourId = "hour-" + dayjs().get('hour');
+
+function createDiv1() {
+
+  $("<div>", {
+    'id': "hour-9",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "9AM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+function createDiv2() {
+
+  $("<div>", {
+    'id': "hour-10",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "10AM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+function createDiv3() {
+
+  $("<div>", {
+    'id': "hour-11",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "11AM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+function createDiv4() {
+
+  $("<div>", {
+    'id': "hour-12",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "12PM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+function createDiv5() {
+
+  $("<div>", {
+    'id': "hour-1",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "1PM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+
+function createDiv6() {
+
+  $("<div>", {
+    'id': "hour-14",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "2PM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+function createDiv7() {
+
+  $("<div>", {
+    'id': "hour-15",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "3PM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+function createDiv8() {
+
+  $("<div>", {
+    'id': "hour-16",
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': "4PM",
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+
+function createDiv9() {
+
+  $("<div>", {
+    'id': "hour-" + dayjs().get('hour'),
+    'class': "row time-block",
+  }).appendTo(this.container)
+    .append($("<div>", {
+      'text': date.format('h A').split(' ').join(''),
+      'class': "col-2 col-md-1 hour text-center py-3",
+    }))
+    .append($("<textarea>", {
+      'rows': "3",
+      'class': "col-8 col-md-10 description",
+    }))
+    .append($("<button>", {
+      'aria-label': "save",
+      'class': "btn saveBtn col-2 col-md-1",
+    }).append($("<i>", {
+      'class': "fas fa-save",
+      'aria-hidden': "true"
+    })))
+
+  saveInput();
+}
+
+
+var cutterntDay = $('#currentDay');
+var saveBtn = document.querySelector('.saveBtn');
+
+var currentHours = dayjs().get('hour');
+
+
+var showCurrentDay = function () {
+  var date = dayjs();
+  $('#currentDay').text(date.format('dddd, MMMM D') + 'th');
+
+}
+
+function getHours() {
+
+  $('.time-block').each(function () {
+    var hoursScheduler = $(this).attr("id").split("hour-")[1];
+    console.log('currentHours  ', currentHours);
+    console.log('hoursScheduler   ', hoursScheduler);
+    if ( hoursScheduler < currentHours) {
+      $(this).addClass('past');
+    } else if (currentHours == hoursScheduler) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+
+  });
+
+
+}
+
+function saveInput() {
+
+  $('.saveBtn').on('click', function () {
+    var hours = $(this).parent().attr("id");
+    console.log(hours);
+    var textAreaValue = $(this).siblings('.description').val();
+    localStorage.setItem(hours, textAreaValue);
+    console.log(textAreaValue);
+  })
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+}
+
+createDiv1();
+createDiv2();
+createDiv3();
+createDiv4();
+createDiv5();
+createDiv6();
+createDiv7();
+createDiv8();
+createDiv9();
+getHours();
+showCurrentDay();
